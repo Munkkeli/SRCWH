bb39c91e79914ed38f442879c2957cab96751605
Broivula <elias.koivula1@hotmail.com>
2019-09-25 23:07:26 +0300

    login get's a succesfull response

--- SRCWH/app/src/main/java/com/example/srcwh/NetworkHandler.kt
+++ SRCWH/app/src/main/java/com/example/srcwh/NetworkHandler.kt
@@ -1,8 +1,12 @@
 package com.example.srcwh
 
+import com.google.gson.Gson
 import com.google.gson.GsonBuilder
+import com.google.gson.JsonObject
+import com.google.gson.annotations.SerializedName
 import okhttp3.*
 import org.jetbrains.anko.doAsync
+import org.json.JSONObject
 import java.io.IOException
 
 class NetworkHandler() {
@@ -10,30 +14,30 @@ class NetworkHandler() {
     private val client = OkHttpClient()
 
     fun postLogin(username: String, password: String) {
-        //first build the fortm
-        val formBody = FormBody.Builder()
-            .add("username", username)
-            .add("password", password)
-            .build()
 
+        val jsonData = JsonObject()
+        jsonData.addProperty("username", username)
+        jsonData.addProperty("password", password)
+        val JSON = MediaType.parse("application/json; charset=utf-8")
+        val b = RequestBody.create(JSON, jsonData.toString())
         val request = Request.Builder()
             .url(LOGIN_URL)
-            .post(formBody)
+            .post(b)
             .build()
 
         try{
             doAsync {
                 val response = client.newCall(request).execute()
                 println("KIKKEL post begins")
+                println("KIKKEL " + response.body()?.string())
                 if(response.isSuccessful){
                     println("KIKKEL response code: " + response.code())
-                    response.code()
                     try {
-                        println("KIKKEL 1")
-                        val gson = GsonBuilder().setPrettyPrinting().create()
-                        println("KIKKEL 2")
-                        val responseJSON = gson.fromJson(response.body().toString(), loginResponse::class.java)
-                        println("KIKKEL " + responseJSON)
+                        //val gson = GsonBuilder().setPrettyPrinting().create()
+                        // the problem currently is, that the gsonbuilder isn't casting the response into loginResponse succesfully.
+                        // the token isn't getting printed
+                        val responseJSON = Gson().fromJson(response.body()?.string(), loginResponse::class.java)
+                        println("KIKKEL " + responseJSON.token)
                        // DatabaseObj.createNewUser(response)
                     }catch (e: IOException){
                         println("KIKKEL something went wrong in casting " + e.toString())}
@@ -45,5 +49,12 @@ class NetworkHandler() {
     }
 }
 
-data class loginResponse(val user: loginUser, val token: Long)
-data class loginUser (val id: Long, val firstname: String, val lastName: String, val groupList: ArrayList<String>, val hash: Long)
+data class loginResponse(
+    @SerializedName("user")val user: loginUser,
+    @SerializedName("token")val token: String)
+data class loginUser (
+    @SerializedName("id")val id: String,
+    @SerializedName("firstname")val firstname: String,
+    @SerializedName("lastname") val lastName: String,
+    @SerializedName("groupList")val groupList: ArrayList<String>,
+    @SerializedName("hash")val hash: String)
